93d769d291b69932bf702f32161fac8f1d87f5a4
uki0604 <marzuki.4122@gmail.com>
2023-02-14 22:28:05 +0700

    ubah dari local

--- soal2.js
+++ soal2.js
@@ -16,7 +16,6 @@ function checkNumber(n) {
     }
   }
     // Test inputs
-  checkNumber(5); //
-  checkNumber(1000);
-  checkNumber(-1000);
-// comment
+  checkNumber(5); // satuan
+  checkNumber(1000); // ribuan
+  checkNumber(-1000); // gak valid
