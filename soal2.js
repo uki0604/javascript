6531342044e85d2f83c42ac515b0bf45b5e86ee3
uki0604 <marzuki.4122@gmail.com>
2023-02-14 20:52:41 +0700

    add jawaban no 2

---
+++ soal2.js
@@ -0,0 +1,21 @@
+function checkNumber(n) {
+    if (n < 0) {
+      console.log("Input tidak valid");
+    } else if (n < 10) {
+      console.log("satuan");
+    } else if (n < 100) {
+      console.log("puluhan");
+    } else if (n < 1000) {
+      console.log("ratusan");
+    } else if (n < 10000) {
+      console.log("ribuan");
+    } else if (n < 100000) {
+      console.log("puluhribuan");
+    } else {
+      console.log("Input tidak valid");
+    }
+  }
+    // Test inputs
+  checkNumber(5); // Output: satuan
+  checkNumber(1000); // Output: ribuan
+  checkNumber(-1000); // Output: Input tidak valid
